daaabbf3544e05f5b66464d5525860b975849c93
r-neal-kelly <r.neal.kelly@gmail.com>
2022-08-11 12:54:15 -0500

    started modeling the ui and model

--- src/index.js
+++ src/index.js
@@ -3,24 +3,73 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class TripleTriad extends React.Component
+import Model from './model.js';
+
+class Game extends React.PureComponent
 {
     constructor(props)
     {
         super(props);
+
+        this.state = {
+            model: new Model(),
+        };
     }
 
     render()
     {
         return (
-            <h1>Hello, World!</h1>
+            <div className="Game">
+                <h1>Triple Triad</h1>
+                <Board tile_count={this.state.model.tile_count} />
+            </div>
+        );
+    }
+}
+
+class Board extends React.PureComponent
+{
+    render()
+    {
+        return (
+            <div className="Board">
+                {Array(this.props.tile_count).fill(null).map((_, index) =>
+                {
+                    return (
+                        <Tile key={index} id={index} />
+                    );
+                })}
+            </div>
+        );
+    }
+}
+
+class Tile extends React.PureComponent
+{
+    render()
+    {
+        return (
+            <div className="Tile">
+                {this.props.id}
+            </div>
+        );
+    }
+}
+
+class Card extends React.PureComponent
+{
+    render()
+    {
+        return (
+            <div />
         );
     }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <TripleTriad />
+        <Game />
     </React.StrictMode>
 );
---
+++ src/model.js
@@ -0,0 +1,9 @@
+export default class Model
+{
+    constructor()
+    {
+        this.row_count = 3;
+        this.column_count = 3;
+        this.tile_count = this.row_count * this.column_count;
+    }
+};
